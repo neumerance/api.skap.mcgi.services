a13bf94db31776217a53c64f33aa3a4a86e49cce
JonJon <neumerance@live.com>
2024-05-11 23:01:20 +0800

    initialize socker server before http server

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ import logger from "./utils/logger.js";
 const PORT = process.env.PORT || 3000;
 let httpServer;
 
+const initializeSocketServer = (httpServer) => {
+  const io = new Server(httpServer, {
+    cors: {
+      origin: ["*"],
+    },
+  });
+
+  io.on("connection", (socket) => {
+    logger.info("connected");
+  });
+};
+
 if (process.env.NODE_ENV === "production") {
   const SSL_PORT = 443;
   const options = {
@@ -15,20 +27,16 @@ if (process.env.NODE_ENV === "production") {
   };
 
   httpServer = https.createServer(options);
+
+  initializeSocketServer(httpServer);
+
   httpServer.listen(SSL_PORT, () => {
     console.log(`[INFO] api.skap.mcgi.app is listening on port ${SSL_PORT}`);
   });
 } else {
   httpServer = http.createServer();
+
+  initializeSocketServer(httpServer);
+
   httpServer.listen(PORT);
 }
-
-const io = new Server(httpServer, {
-  cors: {
-    origin: ["*"],
-  },
-});
-
-io.on("connection", (socket) => {
-  logger.info("connected");
-});
