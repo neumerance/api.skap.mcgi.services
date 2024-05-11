12b3fed8f10fa9aedebced007fceedab08d8c14c
JonJon <neumerance@live.com>
2024-05-11 22:42:59 +0800

    configuring SSL

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-import { createServer } from "http";
+import http from "http";
+import https from "https";
 import { Server } from "socket.io";
 import logger from "./utils/logger.js";
 
 const PORT = process.env.PORT || 3000;
-const httpServer = createServer();
-const io = new Server(httpServer, {});
+let httpServer;
+
+if (process.env.NODE_ENV === "production") {
+  const SSL_PORT = 443;
+  const options = {
+    key: fs.readFileSync("./keys/privkey.pem"),
+    cert: fs.readFileSync("./keys/fullchain.pem"),
+  };
+
+  httpServer = https.createServer(options, app);
+  httpServer.listen(SSL_PORT, () => {
+    console.log(`[INFO] api.skap.mcgi.app is listening on port ${SSL_PORT}`);
+  });
+} else {
+  httpServer = http.createServer();
+  httpServer.listen(PORT);
+}
+
+const io = new Server(httpServer, {
+  cors: {
+    origin: ["*"],
+  },
+});
 
 io.on("connection", (socket) => {
   logger.info("connected");
 });
-
-httpServer.listen(PORT);
